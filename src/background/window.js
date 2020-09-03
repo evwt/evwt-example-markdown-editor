0e9a5b228b5be2d40ab77bef81b9d129305b32b2
John Susek <john@johnsolo.net>
2020-09-02 21:22:01 -0500

    Fix tests

--- src/background/window.js
+++ src/background/window.js
@@ -16,7 +16,7 @@ export function createWindow(restoreId = uuidv4()) {
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      nodeIntegration: process.env.npm_lifecycle_event === 'test' || process.env.ELECTRON_NODE_INTEGRATION,
       preload: path.join(__dirname, 'preload.js')
     }
   };
